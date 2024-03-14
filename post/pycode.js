24c8b8ea9c86f6cb1a928510aa2354dd870712b3
lty <908977590@qq.com>
2024-03-14 20:09:29 +0800

    code style

---
+++ post/pycode.js
@@ -0,0 +1,7 @@
+// prism-setup.js
+
+// 引入 Prism.js 库
+import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/prism.min.js';
+
+// 引入 Python 语言的语法高亮插件
+import 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/components/prism-python.min.js';
